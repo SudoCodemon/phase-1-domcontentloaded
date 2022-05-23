bdf891598a570cb6d1458399220ecccf8d8fa853
Daniel Yang <danyang0724@gmail.com>
2022-05-22 21:00:29 -0400

    commit tree 6afc52becbf1714bffde1ef5b043ad10e5678e6a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const ptext = document.getElementById("text");
+
+    ptext.innerHTML = "This is really cool!"
+})
